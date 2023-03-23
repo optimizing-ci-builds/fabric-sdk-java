4c5fa4cb8f7a7854b6ad01a5a942db86b408f103
optimizing-ci-builds <ocibsummerresearch2022@gmail.com>
2023-03-23 01:03:31 -0400

    Apply both methods to avoid generation of xml logs

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -30,7 +30,7 @@ docker ps -a
 
 export ORG_HYPERLEDGER_FABRIC_SDK_DIAGNOSTICFILEDIR=target/diagDump
 export MAVEN_OPTS="-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn"
-mvn -B clean install -DskipITs=false -Dmaven.test.failure.ignore=false javadoc:javadoc -DstatelessTestsetReporter.disable=true
+mvn -B clean install -DskipITs=false -Dmaven.test.failure.ignore=false javadoc:javadoc -DstatelessTestsetReporter.disable=true -DdisableXmlReport=true
 
 pushd src/test/fixture/sdkintegration/ >/dev/null
 docker-compose down --volumes
